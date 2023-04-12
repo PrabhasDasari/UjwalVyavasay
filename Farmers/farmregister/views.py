4a49f5aae7b21fa3b4710f52989eba704bfe097c
PrabhasDasari <124909746+PrabhasDasari@users.noreply.github.com>
2023-04-12 18:55:36 +0530

    change veiw

--- Farmers/farmregister/views.py
+++ Farmers/farmregister/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponse
 from Home.models import Register
 
 def funroute3(request):
-    if request.method=="POST" :
+    if request.method=="POST":
         print(request)
+        print("hai")
         name=request.POST.get(name,'')
         Register=Register(name=name)
         Register.save()
+    print("hai")
     return render(request,'farmregister/farmregister1.html')
+    
